72316ccdc879a5ca0a12db67904cbf02ac0b7e4c
Karina Nakahara <81200562+nak4hara@users.noreply.github.com>
2021-03-29 23:25:48 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZWFlNzRmOTItZjY1Ni00ZjZkLTk5MDgtOGFhOTYzNWFhYzhmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+var nomeFilmes = ["Toy Story", "Star Wars","Interstellar"]
+
+for(var i = 0; i < listaFilmes.length; i++){
+    document.write("<img src=" + listaFilmes[i] +">")
+    document.write("<h2>" + nomeFilmes[i] + "</h2>")
+}
